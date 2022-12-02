cbacb4e76dd52fc895ec6c5a38e80951a80662ed
Mateusz Lechwar <mateusz.lechwar@topsa.com.pl>
2022-12-02 16:35:03 +0100

    info aktualizacja

--- src/InfoModule/InfoView.tsx
+++ src/InfoModule/InfoView.tsx
@@ -12,6 +12,7 @@ export default class InfoView extends React.Component {
         return(
             <Content>
                 <h2 style={{textAlign: 'center'}}>Informacje</h2>
+                <h4 style={{textAlign: 'center'}}>Strażak który wpisał się na dany dzień, deklaruje dostępność w godzinach <br/><br/>06:00 - 18:00<br/><br/></h4>
                 <p>Dodanie dyspozycyjności na dany dzień <b>gwarantuje</b> miejsce w aucie podczas alarmu w godzinach 06:00 - 18:00.</p>
                 <p>Na dany dzień mogą zapisać się maksymalnie 4 osoby (skład podstawowy) w tym przynajmniej jeden kierowca C.</p>
                 <p>Zgłoszenie dyspozycyjności na dany dzień przez 4 osoby nie oznacza, że inni nie mogą stawić się na alarm. Oznacza to tylko tyle że te cztery osoby mają pierwszeństwo wyjazdu.</p>
--- src/configuration/AppConfiguration.ts
+++ src/configuration/AppConfiguration.ts
@@ -5,7 +5,7 @@ export default class AppConfiguration{
     }
 
     static APP_VER(){
-        return "v1.38"
+        return "v1.39"
     }
 
     static APP_UPDATE(){
